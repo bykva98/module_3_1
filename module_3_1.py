e36309248eff3577c0af306d006eec7f070834d7
bykva98 <bykva.98@mail.ru>
2024-09-24 23:57:56 +0700

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,20 @@
+calls = 0
+def count_calls ():
+    global calls
+    calls += 1
+    return calls
+def string_info (string):
+    count_calls ()
+    return {(len(string)), string.upper(), string.lower()}
+def is_contains (string, list_to_search):
+    count_calls ()
+    if any (substring.lower() in string.lower() for substring in list_to_search):
+        return True
+    else:
+        return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
